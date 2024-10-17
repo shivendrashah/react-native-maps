b8eb426a40646b459628d1b551429cc1914fe503
Shivendra Shah <shivendra.shah@Shivendra-Shah-FVFHK0PPQ05P.local>
2024-10-17 16:28:34 +0530

    Added ios polyline animation

--- ios/AirGoogleMaps/AIRGoogleMapPolylineManager.m
+++ ios/AirGoogleMaps/AIRGoogleMapPolylineManager.m
@@ -14,6 +14,7 @@
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTViewManager.h>
 #import <React/UIView+React.h>
+#import <React/RCTUIManager.h>
 #import "RCTConvert+AirMap.h"
 #import "AIRGoogleMapPolyline.h"
 
@@ -42,6 +43,42 @@ RCT_EXPORT_VIEW_PROPERTY(geodesic, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(zIndex, int)
 RCT_EXPORT_VIEW_PROPERTY(tappable, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+
+RCT_EXPORT_VIEW_PROPERTY(animatedPolyline, AIRGMSPolyline)
+RCT_EXPORT_VIEW_PROPERTY(animateColor, UIColor)
+RCT_EXPORT_VIEW_PROPERTY(drawDone, CGFloat)
+RCT_EXPORT_VIEW_PROPERTY(isAnimating, BOOL)
+
+RCT_EXPORT_METHOD(startPolylineAnimation:(nonnull NSNumber *)reactTag animateColor:(UIColor *)animateColor animationDuration:(CGFloat)duration delay:(CGFloat)delay)
+{
+    [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+        id view = viewRegistry[reactTag];
+        if (![view isKindOfClass:[AIRGoogleMapPolyline class]]) {
+            RCTLogError(@"Invalid view returned from registry, expecting AIRGoogleMapPolyline, got: %@", view);
+        } else {
+            AIRGoogleMapPolyline *polyline = (AIRGoogleMapPolyline *)view;
+            
+            // Start the polyline animation here
+            [polyline startPolylineAnimation:animateColor animationDuration:duration delay:delay];
+        }
+    }];
+}
+
+RCT_EXPORT_METHOD(stopPolylineAnimation:(nonnull NSNumber *)reactTag)
+{
+    [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+        id view = viewRegistry[reactTag];
+        if (![view isKindOfClass:[AIRGoogleMapPolyline class]]) {
+            RCTLogError(@"Invalid view returned from registry, expecting AIRGoogleMapPolyline, got: %@", view);
+        } else {
+            AIRGoogleMapPolyline *polyline = (AIRGoogleMapPolyline *)view;
+            
+            // Stop the polyline animation here
+            [polyline stopPolylineAnimation];
+        }
+    }];
+}
+
 
 @end
 
